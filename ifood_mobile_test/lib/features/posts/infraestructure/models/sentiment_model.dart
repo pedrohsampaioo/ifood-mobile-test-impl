94e40a94052cf6ff9e494203a3b458f4cdbeabc3
pedrohsampaioo <pedrohsampaioo@outlook.com>
2021-04-10 22:48:23 -0300

    feat: Google language service impl

---
+++ ifood_mobile_test/lib/features/posts/infraestructure/models/sentiment_model.dart
@@ -0,0 +1,42 @@
+import '../../domain/entities/sentiment_entity.dart';
+
+class SentimentModel extends SentimentEntity {
+  const SentimentModel({
+    required double score,
+    required double magnitude,
+    required String content,
+    required String language,
+    required String emoji,
+  }) : super(
+          score: score,
+          magnitude: magnitude,
+          content: content,
+          language: language,
+          emoji: emoji,
+        );
+
+  factory SentimentModel.fromJson(Map<String, dynamic> json) {
+    final sentence = json['sentences'][0];
+    final score = sentence['sentiment']['score'];
+    final magnitude = sentence['sentiment']['magnitude'];
+    final content = sentence['text']['content'];
+    final language = json['language'];
+    return SentimentModel(
+      score: score,
+      magnitude: magnitude,
+      content: content,
+      language: language,
+      emoji: _chooseEmoji(score),
+    );
+  }
+
+  static String _chooseEmoji(double score) {
+    if (score == 0) {
+      return '😐';
+    }
+    if (score > 0) {
+      return '😃';
+    }
+    return '😔';
+  }
+}
---
+++ ifood_mobile_test/lib/features/posts/infraestructure/services/google_language_service_impl.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:dartz/dartz.dart';
+
+import '../http_config/http_google_language_decorator.dart';
+import '../../domain/entities/sentiment_entity.dart';
+import '../../domain/failures/analyse_text_failure.dart';
+import '../../domain/services/google_language_service.dart';
+import '../models/sentiment_model.dart';
+
+class GoogleLanguageServiceImpl implements GoogleLanguageService {
+  final HttpGoogleLanguageDecorator _httpClient;
+
+  const GoogleLanguageServiceImpl(this._httpClient);
+
+  @override
+  Future<Either<AnalyseTextFailure, SentimentEntity>> analyzeText(
+    String text,
+  ) async {
+    final body = jsonEncode({
+      'encodingType': 'UTF8',
+      'document': {
+        'type': 'PLAIN_TEXT',
+        'content': text,
+      },
+    });
+    final response = await _httpClient.post(
+      Uri.parse('/v1beta2/documents:analyzeSentiment'),
+      body: body,
+    );
+    if (response.statusCode == 200) {
+      final responseJson = jsonDecode(response.body) as Map<String, dynamic>;
+      final noHasSentence = (responseJson['sentences'] as List).isEmpty;
+      if (noHasSentence) {
+        return left(AnalyseTextFailure.noSentenceEvaluated());
+      }
+      final sentimentEntity = SentimentModel.fromJson(responseJson);
+      return right(sentimentEntity);
+    }
+    return left(AnalyseTextFailure.httpFailure());
+  }
+}
